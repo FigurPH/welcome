24c721a5fa3ca86af16c36910786b8a00721124b
FigurPH <figur.pedro@gmail.com>
2025-11-02 01:27:55 -0300

    0.1.2

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-window.onload = function() {
-    this.alert('carregou tudo');
+async function getPage(page) {
+    if (page == 'navbarDropdownMenuLink' || 
+        page == 'langDropdown'
+    ) return;
+    try {
+        const res = (await fetch(`src/${page}.html`));
+        if (!res.ok) throw new Error(`Erro ao carregar ${page}`);
+        const html = await res.text();
+        document.getElementById('content').innerHTML = html;
+        initPage(page);
+    } catch (err) {
+        console.error(err);
+        document.getElementById('content').innerHTML = `<p>Erro ao carregar ${page}</p>`;
+    }
 }
 
-
-const element_nb_name = document.getElementById('navbar-name');
-if (element_nb_name) {
-    element_nb_name.addEventListener('click', function() {
-        alert('isso é um alerta')
-    });
+// Função de inicialização de ppaginas para casos especiais
+function initPage(page) {
+    if (page === 'whoami') {
+        const anoAtual = new Date().getUTCFullYear();
+        const idadeAtual = anoAtual - 1990;
+        document.getElementById('idade').textContent = idadeAtual;
+    }
 }
 
+// Carrega html correto em content
+document.addEventListener('click', e => {
+    // Busca o id do elemento <a>
+    const link = e.target.closest('a[id]');
+    if (!link) return;
+    e.preventDefault();
+    const page = link.id;
+    // Carrega o conteúdo pertinente
+    getPage(page);
+})
