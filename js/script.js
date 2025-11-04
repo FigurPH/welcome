aee056afbcaeadac6b9d557774bc273f4c393bf6
FigurPH <figur.pedro@gmail.com>
2025-11-03 23:31:51 -0300

    bootstrap5, refatorações, correção de um bug no JS e acho que finalizei por enquanto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 async function getPage(page) {
-    if (page == 'navbarDropdownMenuLink' || 
-        page == 'langDropdown'
-    ) return;
     try {
         const res = (await fetch(`src/${page}.html`, { cache: 'no-store' })); //cache: no-store para ignorar o cache, carrega sempre novamente a página
         if (!res.ok) throw new Error(`Erro ao carregar ${page}`);
         const html = await res.text();
         document.getElementById('content').innerHTML = html;
+    } catch (err) {
+        console.error(err);x
+        document.getElementById('content').innerHTML = `<p>Erro ao carregar ${page}</p>`;
+    }
+}
+async function getPortfolio(page) {
+    try {
+        const res = (await fetch(`src/portfolio/${page}.html`, { cache: 'no-store' })); //cache: no-store para ignorar o cache, carrega sempre novamente a página
+        if (!res.ok) throw new Error(`Erro ao carregar ${page}`);
+        const html = await res.text();
+        document.getElementById('portfolio-content').innerHTML = html;
+
         initPage(page);
     } catch (err) {
         console.error(err);
-        document.getElementById('content').innerHTML = `<p>Erro ao carregar ${page}</p>`;
+        document.getElementById('portfolio-content').innerHTML = `<p>Erro ao carregar ${page}</p>`;
     }
 }
 
@@ -25,11 +34,21 @@ function initPage(page) {
 
 // Carrega html correto em content
 document.addEventListener('click', e => {
+    const pages = [
+        'whoami',
+        'skills',
+        'portfolio'
+    ];
     // Busca o id do elemento <a>
     const link = e.target.closest('a[id]');
     if (!link) return;
     e.preventDefault();
     const page = link.id;
+    if (pages.includes(page)){
+        getPage(page);
+    } else {
+        getPortfolio(page);
+    }
     // Carrega o conteúdo pertinente
-    getPage(page);
+    //getPage(page);
 })
