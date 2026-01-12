f4f49cd1eb0f3db45201097c646da51023432be6
FigurPH <figur.pedro@gmail.com>
2026-01-11 22:27:24 -0300

    initPage alterado

--- js/script.js
+++ js/script.js
@@ -1,14 +1,28 @@
+
+
+
 async function getPage(page) {
     try {
         const res = (await fetch(`src/${page}.html`, { cache: 'no-store' })); //cache: no-store para ignorar o cache, carrega sempre novamente a página
         if (!res.ok) throw new Error(`Erro ao carregar ${page}`);
         const html = await res.text();
-        document.getElementById('content').innerHTML = html;
+        const contentDiv = document.getElementById('content');
+        if (contentDiv) {
+            contentDiv.innerHTML = html;
+            
+            // Verifique se a função existe antes de chamar
+            if (typeof initPage === 'function') {
+                initPage('whoami'); 
+            } else {
+                console.error("Erro: initPage não está definida no escopo global.");
+            }
+        }
     } catch (err) {
-        console.error(err);x
+        console.error(err);
         document.getElementById('content').innerHTML = `<p>Erro ao carregar ${page}</p>`;
     }
 }
+
 async function getPortfolio(page) {
     try {
         const res = (await fetch(`src/portfolio/${page}.html`, { cache: 'no-store' })); //cache: no-store para ignorar o cache, carrega sempre novamente a página
@@ -23,7 +37,7 @@ async function getPortfolio(page) {
     }
 }
 
-// Função de inicialização de ppaginas para casos especiais
+// Função de inicialização de paginas para casos especiais
 function initPage(page) {
     if (page === 'whoami') {
         const anoAtual = new Date().getUTCFullYear();
